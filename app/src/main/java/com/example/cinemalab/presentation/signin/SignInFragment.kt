69b4f74e20531b270c5f0b733613aadbd109e2b8
Solntselikaya <solntselikaya.sofia@yandex.ru>
2023-03-26 12:50:54 +0700

    SignUp layout
    Нужно подумать еще про адаптивность при маленьком экране - добавить прокрутку положив в скроллвью?

---
+++ app/src/main/java/com/example/cinemalab/presentation/signin/SignInFragment.kt
@@ -0,0 +1,23 @@
+package com.example.cinemalab.presentation.signin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.example.cinemalab.R
+
+class SignInFragment : Fragment(R.layout.fragment_sign_in) {
+
+    private val viewModel: SignInViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/cinemalab/presentation/signin/SignInViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.cinemalab.presentation.signin
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel(
+): ViewModel() {
+
+
+}
