0bce593d500b9b8ae0a7ab34d61e4c5e2a593e5d
Solntselikaya <solntselikaya.sofia@yandex.ru>
2023-03-28 00:51:39 +0700

    SignIn ViewModel + mavigation
    Added onButtonClick listeners for navigation, added fields check and AlertDialog

--- app/src/main/java/com/example/cinemalab/presentation/signin/SignInFragment.kt
+++ app/src/main/java/com/example/cinemalab/presentation/signin/SignInFragment.kt
@@ -1,23 +1,78 @@
 package com.example.cinemalab.presentation.signin
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
 import com.example.cinemalab.R
+import com.example.cinemalab.databinding.FragmentSignInBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class SignInFragment : Fragment(R.layout.fragment_sign_in) {
+@AndroidEntryPoint
+class SignInFragment : Fragment() {
 
+    private lateinit var binding: FragmentSignInBinding
     private val viewModel: SignInViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_sign_in, container, false)
+        binding = FragmentSignInBinding.bind(view)
+
+        setOnButtonsClickListeners()
+
+        return binding.root
     }
+
+    private fun createErrorDialog() {
+        val message = viewModel.validationErrorMessage.value
+        val builder = AlertDialog.Builder(context, R.style.AlertDialogTheme)
+
+        builder.setTitle(getString(R.string.input_incorrect))
+        builder.setMessage(message)
+        builder.setOnDismissListener {
+            viewModel.clearErrorMessage()
+        }
+        builder.show()
+    }
+
+    private fun setOnButtonsClickListeners() {
+        setOnRegistrationButtonClickListener()
+        setOnSignInButtonClickListener()
+    }
+
+    private fun setOnSignInButtonClickListener() {
+        binding.btSignIn.setOnClickListener {
+
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            viewModel.checkIfFieldsIsValid(
+                email,
+                password
+            )
+
+            if (viewModel.allFieldsIsValid.value == false)
+                createErrorDialog()
+            else
+                viewModel.login(email, password)
+        }
+    }
+
+    private fun setOnRegistrationButtonClickListener() {
+        binding.btRegistration.setOnClickListener {
+            navigateToSignUpScreen()
+        }
+    }
+
+    private fun navigateToSignUpScreen() {
+        findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
+    }
+
 }
--- app/src/main/java/com/example/cinemalab/presentation/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/cinemalab/presentation/signin/SignInViewModel.kt
@@ -1,11 +1,66 @@
 package com.example.cinemalab.presentation.signin
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import android.content.Context
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cinemalab.R
+import com.example.cinemalab.data.remote.dto.LoginDto
+import com.example.cinemalab.domain.usecase.LoginUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SignInViewModel(
+@HiltViewModel
+class SignInViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val loginUseCase: LoginUseCase
 ): ViewModel() {
 
+    private val _allFieldsIsValid = MutableLiveData(true)
+    var allFieldsIsValid: LiveData<Boolean> = _allFieldsIsValid
+
+    private val _validationErrorMessage = MutableLiveData("")
+    var validationErrorMessage: LiveData<String> = _validationErrorMessage
+
+    fun clearErrorMessage() {
+        _validationErrorMessage.value = ""
+    }
+
+    fun checkIfFieldsIsValid(
+        email: String,
+        password: String
+    ) {
+        if (email.isBlank() || password.isBlank()) {
+            _validationErrorMessage.value += context.getString(R.string.fill_all_fields)
+        }
+        _allFieldsIsValid.value = checkIfEmailIsValid(email) && password.isNotBlank()
+    }
+
+    private fun checkIfEmailIsValid(email: String): Boolean {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+            _validationErrorMessage.value += context.getString(R.string.invalid_email_error_message)
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun login(email: String, password: String) {
+
+        val body = LoginDto(
+            email = email,
+            password = password
+        )
+
+        viewModelScope.launch {
+            try {
+                val token = loginUseCase(body)
+
+            } catch(ex: Exception) {
+                //TODO
+            }
+        }
+    }
 
 }
