ee0e73f4cd3843e3bf6a0d6dc9d7dd0c93ccab97
Solntselikaya <solntselikaya.sofia@yandex.ru>
2023-03-28 19:55:50 +0700

    started researching navigation
    Проблемы огромные с навигацией, не совсем понятно как это делать пока что..

---
+++ app/src/main/java/com/example/cinemalab/presentation/profile/ProfileViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.cinemalab.presentation.profile
+
+import android.content.Context
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val
+) {
+}
